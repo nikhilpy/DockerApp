a57e8f68f2579f2d7860007c53a2d09078ad1a1b
nikhilpy <nikhil.belalekar@gmail.com>
2023-07-14 11:00:44 +0530

    Server.js updated with mongoURL module

--- app/server.js
+++ app/server.js
@@ -25,7 +25,7 @@ app.get('/profile-picture', function (req, res) {
 
 // use when starting application as docker container
 
-let mongoUrlDocker = "mongodb://admin:password@mongodb";
+let mongoUrlLocal = "mongodb://admin:password@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
